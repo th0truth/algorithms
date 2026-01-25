9283fa24dec470dd9a0fd0e59be48cf019ceeac0
the truth <shiftrams@gmail.com>
2026-01-25 12:08:44 +0200

    Refactor selection sort and improve array initialization

--- sorting/selection_sort.cpp
+++ sorting/selection_sort.cpp
@@ -1,49 +1,45 @@
 #include <iostream>
 #include <cstdint>
+#include <ostream>
 #include <vector>
-#include <random>
+#include <string>
+#include <chrono>
 #include <ctime>
 using namespace std;
 
-void Selection_Sort(int32_t *array, int size); 
+void selection_sort(int32_t * array, int size); 
+void init_array(vector<int32_t>& array, int n, const char ** argv);
 
-int main(int argc, const char **argv)
+int main(int argc, const char ** argv)
 {
-  int size = argc-1;
-  vector<int32_t> array(size);
+  vector<int32_t> array;
   
-  for (int i = 1; i < argc; i++)
-  {
-    array[i-1] = stoi(argv[i]);
-  }
+  // Initialize array
+  init_array(array, argc, argv);
 
-  if (array.data() == 0)
-  {
-    int size;
-    cin >> size;
-    
-    mt19937 random(time(nullptr));
-    
-    
-  }
-  
+  // Measure duration time of selection sort execution
+  chrono::steady_clock::time_point begin = chrono::steady_clock::now();
+  selection_sort(array.data(), array.size());
+  chrono::steady_clock::time_point end = chrono::steady_clock::now();
 
-  Selection_Sort(array.data(), size);
-
-  cout << "Result: ";
-  for (int val : array)
+  cout << "\nSorted: ";
+  for (int32_t val : array)
   {
     cout << val << " ";
   }
-  cout << endl;
+
+  cout << "\nDuration time: " << chrono::duration_cast<chrono::nanoseconds>(end - begin).count() << " ns" << endl;
 
   return 0;
 }
 
-// Selection Sort (but it works slightly different than a traditional)
-void Selection_Sort(int32_t *array, int size)
+// Selection Sort
+void selection_sort(int32_t * array, int size)
 {
-  for (int32_t j = size - 1; j >= 0; j--)
+  cout << "Start selection sorting ...\n" << endl;
+  int counter = 0;
+
+  for (int j = size - 1; j >= 0; j--)
   {
     int32_t _max = array[j];
     int idx = j;
@@ -55,9 +51,56 @@ void Selection_Sort(int32_t *array, int size)
         _max = array[i];
         idx = i;
       }
+    }    
+
+    cout << "\tMax found: " << _max << " at index " << idx << endl;
+    
+
+    if (idx != j)
+    {
+      cout << "\tSwap " << array[j] << " at " << j << " with " << array[idx] << "\n\n";
+      counter++;
+    } else {
+      cout << "\tNo swap needed\n\n";
     }
-    int32_t tmp = array[j];
-    array[j] = array[idx];
-    array[idx] = tmp;
+
+    // swap using build in function std::swap
+    swap(array[j], array[idx]);
+    
+    cout << "\tArray now: ";
+    for (int k = 0; k < size; k++)
+    {
+      cout << array[k] << " ";
+    }
+    cout << endl;
+  }
+  cout << "\nTotal swaps: " << counter << endl;
+}
+
+void init_array(vector<int32_t>& array, int n, const char ** argv)
+{
+  if (n > 1)
+  {
+    cout << "Unsorted: ";
+    for (int i = 1; i < n; i++)
+    {
+      array.push_back(stoi(argv[i]));
+      cout << array.back() << " ";
+    }
+    cout << "\n\n";
+  } else {
+    int size;
+
+    cout << "Size of array: ";
+    cin >> size;
+    srand(time(nullptr));
+    
+    cout << "Unsorted: ";
+    for (int i = 0; i < size; i++)
+    {
+      array.push_back(rand());
+      cout << array.back() << " ";
+    }
+    cout << "\n\n";
   }
 }
