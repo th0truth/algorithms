b107dbd633ec5dc86c949c127fdc35a9b9a1d754
th0truth <shiftrams@gmail.com>
2026-03-25 21:28:43 +0200

    refactor: update

--- include/cpu/search.h
+++ include/cpu/search.h
@@ -2,7 +2,9 @@
 #include "base.h"
 using namespace std;
 
-namespace cpu::search {
-  int LinearSearch(i32* array, int size, i32 target, bool visualize = false, chrono::steady_clock::time_point start_time = chrono::steady_clock::now());
-  int BinarySearch(i32* array, int size, i32 target, bool visualize = false, chrono::steady_clock::time_point start_time = chrono::steady_clock::now());
+namespace cpu {
+  namespace search {
+    int LinearSearch(i32* array, int size, i32 target, bool visualize = false, chrono::steady_clock::time_point start_time = chrono::steady_clock::now());
+    int BinarySearch(i32* array, int size, i32 target, bool visualize = false, chrono::steady_clock::time_point start_time = chrono::steady_clock::now());
+  }
 }
--- include/cpu/sort.h
+++ include/cpu/sort.h
@@ -2,13 +2,15 @@
 #include "base.h"
 using namespace std;
 
-namespace cpu::sort {
-  void SelectionSort(i32* array, int size, bool visualize = false, chrono::steady_clock::time_point start_time = chrono::steady_clock::now()); 
-  void InsertionSort(i32* array, int size, bool visualize = false, chrono::steady_clock::time_point start_time = chrono::steady_clock::now());
-  void BubbleSort(i32* array, int size, bool visualize = false, chrono::steady_clock::time_point start_time = chrono::steady_clock::now());
-  void CocktailSort(i32* array, int size, bool visualize = false, chrono::steady_clock::time_point start_time = chrono::steady_clock::now());
-  void HeapSort(i32* array, int size, bool visualize = false, chrono::steady_clock::time_point start_time = chrono::steady_clock::now());
-  void MergeSort(i32* array, int size, bool visualize = false, chrono::steady_clock::time_point start_time = chrono::steady_clock::now());
-  void QuickSort(i32* array, int size, bool visualize = false, chrono::steady_clock::time_point start_time = chrono::steady_clock::now());
-  void ShellSort(i32* array, int size, bool visualize = false, chrono::steady_clock::time_point start_time = chrono::steady_clock::now());
+namespace cpu {
+  namespace sort {
+    void SelectionSort(i32* array, int size, bool visualize = false, chrono::steady_clock::time_point start_time = chrono::steady_clock::now()); 
+    void InsertionSort(i32* array, int size, bool visualize = false, chrono::steady_clock::time_point start_time = chrono::steady_clock::now());
+    void BubbleSort(i32* array, int size, bool visualize = false, chrono::steady_clock::time_point start_time = chrono::steady_clock::now());
+    void CocktailSort(i32* array, int size, bool visualize = false, chrono::steady_clock::time_point start_time = chrono::steady_clock::now());
+    void HeapSort(i32* array, int size, bool visualize = false, chrono::steady_clock::time_point start_time = chrono::steady_clock::now());
+    void MergeSort(i32* array, int size, bool visualize = false, chrono::steady_clock::time_point start_time = chrono::steady_clock::now());
+    void QuickSort(i32* array, int size, bool visualize = false, chrono::steady_clock::time_point start_time = chrono::steady_clock::now());
+    void ShellSort(i32* array, int size, bool visualize = false, chrono::steady_clock::time_point start_time = chrono::steady_clock::now());
+  }
 }
